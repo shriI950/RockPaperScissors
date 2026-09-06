4a3b8d900c74a770fe4701e29d4f59df9f6f65ba
Shrirang Joshi <51706802+shriI950@users.noreply.github.com>
2026-09-06 11:05:27 +0530

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,30 +1,54 @@
 import random
 
-print("Welcome to game of Rock, Paper, Scissors!!!")
+print("Welcome to game of Rock, Paper, Scissors, Lizard, Spock!!!")
 
 print("1 is for '✊' (Rock).")
 print("2 is for '✋' (Paper).")
 print("3 is for '✌️' (Scissors).")
+print("4 is for '🦎' (Lizard).")
+print("5 is for '🖖' (Spock).")
 
-options = ['✊', '✋', '✌️']
-player = int(input("Select number between 1 to 3: "))
-computer = random.randint(1,3)
+options = ['✊', '✋', '✌️', '🦎', '🖖']
+player_won = False # Program runs till Player wins!
 
-if player==computer:
-  print("It's a tie!")
-else:
-  if player==1:
-    if computer==2:
-      print("The computer won!")
-    else:
-      print("The player won!")
-  elif player==2:
-    if computer==1:
-      print("The player won!")
-    else:
-      print("The Computer won!")
+while not player_won:
+  player = int(input("Select number between 1 to 5: "))
+  computer = random.randint(1,5)
+  
+  print(f"You chose: {options[player-1]}")
+  print(f"CPU chose: {options[computer-1]}")
+  
+  
+  if player==computer:
+    print("It's a tie!")
   else:
-    if computer==1:
-      print("The computer won!")
+    if player==1:
+      if computer==2 or computer==5:
+        print("The computer won!")
+      else:
+        print("The player won!")
+        player_won=True
+    elif player==2:
+      if computer==3 or computer==4:
+        print("The computer won!")
+      else:
+        print("The player won!")
+        player_won=True
+    elif player==3:
+      if computer==1 or computer==5:
+        print("The computer won!")
+      else:
+        print("The player won!")
+        player_won=True
+    elif player==4:
+      if computer==1 or computer==3:
+        print("The computer won!")
+      else:
+        print("The player won!")
+        player_won=True
     else:
-      print("The player won!")
+      if computer==4 or computer==2:
+        print("The computer won!")
+      else:
+        print("The player won!")
+        player_won=True
